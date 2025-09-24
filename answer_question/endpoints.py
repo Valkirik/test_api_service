b173b0d645ebd4ad91661a185e3574335d4a23a3
Valery <valkirik1995@mail.ru>
2025-09-24 21:12:30 +0300

    creating endpoints for question model

--- answer_question/endpoints.py
+++ answer_question/endpoints.py
@@ -1,8 +1,9 @@
 from .models import Questions, Answers
 from .serializers import QuestionsSerializer, AnswersSerializer
 from rest_framework import permissions
-from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, DestroyAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, DestroyAPIView, get_object_or_404
 
+"QUESTION"
 
 class QuestionListAPIView(ListAPIView):
     serializer_class = QuestionsSerializer
@@ -26,3 +27,27 @@ class QuestionDeleteAPIView(DestroyAPIView):
     serializer_class = QuestionsSerializer
     permission_classes = [permissions.AllowAny]
     queryset = Questions.objects.all()
+
+
+"ANSWERS"
+
+class AnswerCreateAPIview(CreateAPIView):
+    serializer_class = AnswersSerializer
+    permission_classes = [permissions.AllowAny]
+    queryset = Answers.objects.all()
+
+    def perform_create(self, serializer):
+        question = get_object_or_404(Questions, pk=self.kwargs["pk"])
+        serializer.save(question_id=question)
+
+
+class AnswerRetrieveAPIView(RetrieveAPIView):
+    serializer_class = AnswersSerializer
+    permission_classes = [permissions.AllowAny]
+    queryset = Answers.objects.all()
+
+
+class AnswerDeleteAPIView(DestroyAPIView):
+    serializer_class = AnswersSerializer
+    permission_classes = [permissions.AllowAny]
+    queryset = Answers.objects.all()
